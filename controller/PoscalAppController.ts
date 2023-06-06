8bc4fcb9e3057aa323fc5e98036669f6fc7c7b33
Goldjay94 <genevieveezeh786@gmail.com>
2023-06-06 16:16:56 +0000

    set structure

---
+++ controller/PoscalAppController.ts
@@ -0,0 +1,23 @@
+import { db } from "../utils/db.server";
+import redisClient from "../utils/redis.server";
+import { Request, Response } from "express";
+
+class PoscalAppController {
+    async getStatus(req: Request, res: Response) {
+        let dbStatus = false
+        try {
+            await db.$connect()
+            await db.user.findMany()
+            // await db.$queryRaw('SELECT 1')
+            // console.log('Prisma client is okay')
+            dbStatus = true
+        } catch (error) {
+            dbStatus = false
+        }
+
+        res.status(200).json({ db: true, redis: redisClient.isAlive() })
+    }
+}
+
+const PoscalappController = new PoscalAppController();
+export default PoscalappController;
